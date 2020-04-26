3c5c7cb1d0875f5068fb5dd15aba7e7d71859987
Mubeen Afzal <mubeenafzal3@gmail.com>
2020-04-26 07:51:08 -0700

    react hook dala ha or ak button

--- App.js
+++ App.js
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableWithoutFeedback, Button } from 'react-native';
 
-export default function App() {
+const App =() => {
   const buttonPress= (msg) => {
     alert("Ya corona kuch nae ha sb "+msg+" sazish ha!")
-
   }
+  const [getDeText,setDeText] = useState('Press CHANGE!')
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        Muhammad Mubeen Afzal
-      </Text>
-      <Text style={styles.title}>
-        FA17-BCS-056
-      </Text>
-      <Text style={styles.about}>
-      Ma nahi btaon ga!!
       
-      </Text>
-      <Button title='Corona?' onPress={buttonPress.bind("yahodi")}>
-        
-      </Button>
+        <Text style={styles.title}>
+          Muhammad Mubeen Afzal
+        </Text>
+        <Text style={styles.title}>
+          {getDeText}
+        </Text>
+        <Text style={styles.about}>
+        Ma nahi btaon ga!!
+        </Text>
+        <View style={{flexDirection : 'row'}}>
+          <View style={{marginRight:10}}>
+            <Button title='Corona?' onPress={buttonPress.bind(this,"yahodi")} style={styles.button}></Button>
+          </View>
+          <View>
+            <Button title='Change?' onPress={()=>setDeText("FA17-BCS-056")}></Button>
+          </View>
+        </View>
     </View>
   );
 }
+export default App;
 
 const styles = StyleSheet.create({
   container: {
+    
     flex: 1,
-    backgroundColor: '#333',
+    backgroundColor: '#006669',
     alignItems: 'center',
     color: '#fff',
     justifyContent: 'center',
@@ -43,6 +51,16 @@ const styles = StyleSheet.create({
     paddingBottom:25,
   },
   button :{
-    backgroundColor:'black',
-  }
+    flex:1,
+    margin:20,
+  },
+  
 });
+//const image = require("C:/Users/mubee/expoApp/bg.jpg");
+// <ImageBackground source={image} style={styles.image}>
+//     </ImageBackground>
+// image :{
+//   flex: 1,
+//   resizeMode: "cover",
+//   justifyContent: "center"
+// }
