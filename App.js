6d16b4a87be87ab101435e3c3dfee75fe7be8b3d
Mubeen Afzal <mubeenafzal3@gmail.com>
2020-05-09 05:42:06 -0700

    Clear all and add background

--- App.js
+++ App.js
@@ -1,66 +1,32 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableWithoutFeedback, Button } from 'react-native';
+import React from 'react';
+import {ImageBackground, StyleSheet, Text, View } from 'react-native';
 
-const App =() => {
-  const buttonPress= (msg) => {
-    alert("Ya corona kuch nae ha sb "+msg+" sazish ha!")
-  }
-  const [getDeText,setDeText] = useState('Press CHANGE!')
-  
+
+const image = require("C:/Users/mubee/expoApp/bg.jpg");
+
+export default function App(){
   return (
     <View style={styles.container}>
-      
-        <Text style={styles.title}>
-          Muhammad Mubeen Afzal
-        </Text>
-        <Text style={styles.title}>
-          {getDeText}
-        </Text>
-        <Text style={styles.about}>
-        Ma nahi btaon ga!!
-        </Text>
-        <View style={{flexDirection : 'row'}}>
-          <View style={{marginRight:10}}>
-            <Button title='Corona?' onPress={buttonPress.bind(this,"yahodi")} style={styles.button}></Button>
-          </View>
-          <View>
-            <Button title='Change?' onPress={()=>setDeText("FA17-BCS-056")}></Button>
-          </View>
-        </View>
+      <ImageBackground source={image} style={styles.image}>
+        <Text style={styles.text}>What is this</Text>
+      </ImageBackground>
     </View>
   );
 }
-export default App;
 
 const styles = StyleSheet.create({
   container: {
-    
     flex: 1,
-    backgroundColor: '#006669',
-    alignItems: 'center',
-    color: '#fff',
-    justifyContent: 'center',
+    flexDirection: "column"
   },
-  title:{
-    fontSize: 26,
-    color: '#fff',
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
   },
-  about:{
+  text: {
+    color: "black",
     fontSize: 30,
-    color: 'yellow',
-    paddingBottom:25,
-  },
-  button :{
-    flex:1,
-    margin:20,
-  },
-  
+    fontWeight: "bold"
+  }
 });
-//const image = require("C:/Users/mubee/expoApp/bg.jpg");
-// <ImageBackground source={image} style={styles.image}>
-//     </ImageBackground>
-// image :{
-//   flex: 1,
-//   resizeMode: "cover",
-//   justifyContent: "center"
-// }
